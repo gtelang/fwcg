9fdbc605c4686537d4bbe39e67e67f56e27e36a7
Gaurish Telang <gaurish108@gmail.com>
2018-10-26 11:19:51 -0400

    First Commit

---
+++ img/transfer_files.sh
@@ -0,0 +1,16 @@
+cd /home/gaurish/Dropbox/MyWiki/research_projects/HorseFly/code_horseflies/NumSites-6-CompareAlgos2018-07-19---03-50-56.800646
+cp frame-exact-00000.png \
+   frame-exact-00010.png \
+   frame-exact-00020.png \
+   frame-exact-00030.png \
+   frame-exact-00040.png \
+   frame-exact-00050.png \
+   frame-exact-00060.png \
+   frame-exact-00070.png \
+   frame-exact-00080.png \
+   frame-exact-00090.png \
+   frame-exact-00100.png \
+   frame-exact-00110.png \
+   frame-exact-00120.png \
+   frame-exact-00130.png \
+   frame-exact-00140.png /home/gaurish/Dropbox/MyWiki/research_projects/HorseFly/code_horseflies/report_July2018_DARPA/img
---
+++ slides/compile.sh
@@ -0,0 +1,4 @@
+pdflatex \\nonstopmode \\input slides.tex
+bibtex slides.aux
+pdflatex \\nonstopmode \\input slides.tex
+pdflatex \\nonstopmode \\input slides.tex
